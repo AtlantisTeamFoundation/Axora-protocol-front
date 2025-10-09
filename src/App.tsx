8dbb83c4a566ecfce343a4df7a81a6440f637726
farazmn <farazmnm@gmail.com>
2025-10-09 17:38:25 +0330

    fix : fix ./pages/whitePaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Home from './home'
-import AirDrop from './pages/airdrop'
-import Ico from './pages/ico'
-import Stacking from './pages/stacking'
-import WhitePaper from './pages/whitepaper'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./home";
+import AirDrop from "./pages/airdrop";
+import Ico from "./pages/ico";
+import Stacking from "./pages/stacking";
+import WhitePaper from "./pages/whitePaper";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/pages/airdrop' element={<AirDrop />}/>
-        <Route path='/pages/ico' element={<Ico />}/>
-        <Route path='/pages/stacking' element={<Stacking />}/>
-        <Route path='/pages/whitepaper' element={<WhitePaper />}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pages/airdrop" element={<AirDrop />} />
+          <Route path="/pages/ico" element={<Ico />} />
+          <Route path="/pages/stacking" element={<Stacking />} />
+          <Route path="/pages/whitepaper" element={<WhitePaper />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
