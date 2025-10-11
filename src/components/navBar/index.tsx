003a1f49830f51f852b3fcb452aac8ebeac2ecb4
Esmaiel <esidev98@gmail.com>
2025-10-11 21:38:49 +0330

    fit : added dark button and dark mode colors to navbar

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import DarkModeBtn from "../darkModeBtn";
 
 const Navbar = () => {
   // State to manage the navbar's visibility
@@ -9,6 +10,14 @@ const Navbar = () => {
   // Toggle function to handle the navbar's display
   const handleNav = () => {
     setNav(!nav);
+  };
+
+  // State to control dark mode
+  const [dark, setDark] = useState<boolean>(false);
+
+  // function to control dark mode
+  const darkModeHandler = () => {
+    setDark(!dark);
   };
 
   // Array containing navbar items
@@ -21,15 +30,31 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-blue-400 flex justify-between items-center h-24 mx-auto px-4 text-lg text-white">
+    <nav
+      className={`${
+        dark ? "bg-[#1c1f27] text-[#e0e0e0]" : "bg-blue-400 text-white"
+      } flex justify-between items-center h-24 mx-auto px-4 text-lg`}
+    >
       {/* Logo */}
-      <h1 className="w-auto text-3xl font-bold text-[#0026ff]">Axora</h1>
+      <h1
+        className={`${
+          dark ? "text-[#00bcd4]" : "text-[#0026ff]"
+        } w-auto mr-auto text-3xl font-bold`}
+      >
+        Axora
+      </h1>
 
       {/* Desktop Navbar */}
       <ul className="hidden md:flex">
         {navItems.map((item) => (
           <Link key={item.id} to={item.path}>
-            <li className="p-2.5 hover:bg-[#62d7f5] rounded-xl mx-2 cursor-pointer duration-300 hover:text-[#0026ff]">
+            <li
+              className={`${
+                dark
+                  ? "hover:text-black hover:bg-[#3aabbb]"
+                  : "hover:text-[#0026ff] hover:bg-[#62d7f5]"
+              } p-2.5  rounded-xl mx-2 cursor-pointer duration-300 `}
+            >
               {item.text}
             </li>
           </Link>
@@ -39,26 +64,45 @@ const Navbar = () => {
       {/* Mobile Navbar Icon */}
       <div
         onClick={handleNav}
-        className="block md:hidden cursor-pointer hover:bg-[#62d7f5] p-2 rounded-sm"
+        className={`${
+          dark ? "hover:bg-[#3aabbb]" : "hover:bg-[#62d7f5]"
+        } block md:hidden cursor-pointer p-2 rounded-sm mr-1`}
       >
         {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
       </div>
+
+      {/* Dark mode button */}
+      <DarkModeBtn dark={dark} darkModeHandler={darkModeHandler} />
 
       {/* Mobile Navbar Menu */}
       <ul
         className={
           nav
-            ? "fixed md:hidden left-0 top-0 w-[60%] h-full bg-blue-400 ease-in-out duration-500"
+            ? `fixed md:hidden left-0 top-0 w-[60%] h-full ease-in-out duration-500 ${
+                dark ? "bg-[#1c1f27] text-[#e0e0e0]" : "bg-blue-400 text-white"
+              }`
             : "ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]"
         }
       >
         {/* Mobile Logo */}
-        <h1 className="w-full text-3xl font-bold text-[#0026ff] m-4">Axora</h1>
+        <h1
+          className={`${
+            dark ? "text-[#00bcd4]" : "text-[#0026ff]"
+          } w-full text-3xl font-bold m-4`}
+        >
+          Axora
+        </h1>
 
         {/* Mobile Navbar Items */}
         {navItems.map((item) => (
           <Link key={item.id} to={item.path}>
-            <li className="p-4 border-b hover:bg-[#62d7f5] duration-300 hover:text-[#0026ff] cursor-pointer border-blue-600">
+            <li
+              className={`${
+                dark
+                  ? "hover:bg-[#3aabbb] hover:text-black border-[#3b5168]"
+                  : "hover:bg-[#62d7f5] hover:text-[#0026ff] border-blue-600"
+              } p-4 border-b  duration-300 cursor-pointer`}
+            >
               {item.text}
             </li>
           </Link>
